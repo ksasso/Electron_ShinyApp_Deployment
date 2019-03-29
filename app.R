6a9da061ac653b9f6419b27d7a1a848074ca7634
ramakantsharma <ramakant15sharma08@gmail.com>
2019-03-29 15:29:55 +0530

    Update app.R

--- app.R
+++ app.R
@@ -1,91 +1,32 @@
 library(shiny)
+library(data.table)
+library(DT)
 
-
-shinyApp(
- shinyUI(
-# Define UI for application that draws a histogram
-
-fluidPage(
-
-
-
-  # Application title
-
-  titlePanel("Hello Shiny!"),
-
-
-
-  # Sidebar with a slider input for the number of bins
-
+ui <- fluidPage(
+  titlePanel("Multiple file uploads"),
   sidebarLayout(
-
     sidebarPanel(
-
-      sliderInput("bins",
-
-                  "Number of bins:",
-
-                  min = 1,
-
-                  max = 50,
-
-                  value = 30)
-
+      fileInput("csvs",
+                label="Upload CSVs here",
+                accept = c("text/csv",
+                           "text/comma-separated-values,text/plain",
+                           ".csv"),
+                multiple = TRUE)
     ),
-
-
-
-    # Show a plot of the generated distribution
-
     mainPanel(
-
-      plotOutput("distPlot")
-
+      DT::dataTableOutput("mytable")
     )
-
   )
-
 )
 
-),
-shinyServer(
-
-function(input, output) {
-
-
-
-  # Expression that generates a histogram. The expression is
-
-  # wrapped in a call to renderPlot to indicate that:
-
-  #
-
-  #  1) It is "reactive" and therefore should be automatically
-
-  #     re-executed when inputs change
-
-  #  2) Its output type is a plot
-
-
-
-  output$distPlot <- renderPlot({
-
-    x    <- faithful[, 2]  # Old Faithful Geyser data
-
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-
-
-    # draw the histogram with the specified number of bins
-
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
+server <- function(input, output) {
+  mycsvs<-reactive({
+    rbindlist(lapply(input$csvs$datapath, fread),
+              use.names = TRUE, fill = TRUE)
   })
-
-
-
+  output$mytable = DT::renderDataTable({
+    mycsvs()
+  })
 }
 
-)
-
-)
+shinyApp(ui = ui, server = server)
